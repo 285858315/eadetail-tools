5993347e001ae9027685f21c24a56edc635e0730
xxn <285858315@qq.com>
2019-02-19 00:16:42 +0800

    update

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,6 +1,6 @@
 <template>
 <div>
-    index...
+    <div id='c1'></div>
 </div>
 </template>
 
@@ -30,24 +30,43 @@ export default {
             return res.map(item => item.split("	")).map(item => {
                 return item.reduce((a,b,index) => {
                     a = a || {}
+                    switch(field[index]){
+                        case "date":
+                            // b = new Date(b)
+                            break;
+                        case "open":
+                            b = parseFloat(b)
+                            break;
+                    }
                     a[field[index]] = b
                     return a
                 },{})
             })
         },
         async update(){
-            let eurgbp = await this.parseData("history/EURGBP_D1.csv")
-            let eurusd = await this.parseData("history/EURUSD_D1.csv")
-            let gbpusd = await this.parseData("history/GBPUSD_D1.csv")
+            let eurgbp = await this.parseData("history/EURGBP_W1.csv")
+            let eurusd = await this.parseData("history/EURUSD_W1.csv")
+            let gbpusd = await this.parseData("history/GBPUSD_W1.csv")
             console.log(eurusd[0])
             console.log(gbpusd[0])
-            let ds = new DataSet({
-                state: {
-                    start: new Date(eurgbp[0].date).getTime(),
-                    end: new Date(eurgbp[10].date).getTime()
-                }
-            })
-            console.log(ds)
+            // let ds = new DataSet({
+            //     state: {
+            //         start: new Date(eurgbp[0].date).getTime(),
+            //         end: new Date(eurgbp[10].date).getTime()
+            //     }
+            // })
+            // console.log(ds)
+
+            let data = eurusd.slice(0,1000)
+            console.log(data)
+            var chart = new G2.Chart({
+                container: 'c1',
+                forceFit: true,
+                height : 400
+            });
+            chart.source(data);
+            chart.interval().position('date*open').color('#CCC')
+            chart.render();
         }
     }
 }
